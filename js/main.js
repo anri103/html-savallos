17ba1ecae160e659439a598c7054781e7c1df46a
anri103 <ianrianri@gmail.com>
2023-05-30 23:07:31 +0300

    footer and header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const toggleButtons = document.querySelectorAll('.menu-item-has-children');
+
+(() => {
+    toggleButtons.forEach((toggleButton) => {
+        toggleButton.addEventListener('click', () => {
+            toggleButton.lastElementChild.classList.toggle('open');
+        });
+    });
+})();
+
 //////////////////////////////////////////////////////////////////
 // [ Главный слайдер ]
 
@@ -12,41 +22,6 @@ const heroSwiper = new Swiper('.heroSwiper', {
     },
 });
 
-// На shop-item.html
-const thumbsSwiper = new Swiper('.thumbsSwiper', {
-    spaceBetween: 8,
-    slidesPerView: 6,
-});
-const productItemSwiper = new Swiper('.productItemSwiper', {
-    slidesPerView: 1.2,
-    spaceBetween: 12,
-    grabCursor: true,
-    thumbs: {
-        swiper: thumbsSwiper,
-    },
-    breakpoints: {
-        992: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-        },
-    },
-});
-
-//////////////////////////////////////////////////////////////////
-// [ PHONE MASK ]
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    var mask1 = document.getElementById('maskPhone1')
-    if (mask1) {
-        const maskPhone1 = IMask(
-            mask1, {
-            mask: '+{7} (000) 000-00-00'
-        });
-    }
-
-});
-
 //////////////////////////////////////////////////////////////////
 // [ Enable Fancybox ]
 
@@ -54,7 +29,3 @@ Fancybox.bind("[data-fancybox]", {
     // Your custom options
 });
 
-//////////////////////////////////////////////////////////////////
-// [ Включаем Bootstrap подсказки ]
-
-[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(function (e) { return new bootstrap.Tooltip(e, { trigger: "hover" }) })
